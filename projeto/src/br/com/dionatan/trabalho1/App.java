9b6ae4f2a36a523dbcb54cce7d94313a92bda4dd
dionatamsouza <dionatamsouza@yahoo.com.br>
2020-11-26 22:32:10 -0300

    Create App.java

---
+++ projeto/src/br/com/dionatan/trabalho1/App.java
@@ -0,0 +1,16 @@
+package br.com.dionatan.trabalho1;
+
+
+import br.com.dionatan.trabalho1.models.Contato;
+import br.com.dionatan.trabalho1.models.Endereco;
+
+
+public class App {
+    
+    public static void main( String[] args ) {
+        
+        System.out.println("teste ok");
+        
+    }
+    
+}
