e12665c20f4b1be6a0859683235f9b86fa95dc8b
dionatamsouza <dionatamsouza@yahoo.com.br>
2020-11-28 19:06:45 -0300

    Update App.java

--- projeto/src/br/com/dionatan/trabalho1/App.java
+++ projeto/src/br/com/dionatan/trabalho1/App.java
@@ -40,3 +40,16 @@ public class App {
     }
     
 }
+
+
+/*
+    Scanner sc = new Scanner(System.in);
+
+    float numF = sc.nextFloat();
+    int num1 = sc.nextInt();
+    byte byte1 = sc.nextByte();
+    long lg1 = sc.nextLong();
+    boolean b1 = sc.nextBoolean();
+    double num2 = sc.nextDouble();
+    String nome = sc.nextLine();
+*/
